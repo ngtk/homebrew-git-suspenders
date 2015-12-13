9184279e87aa5d74eab5dcccaeabeac6321a081e
Kent Nagata <ngtknt@me.com>
2015-12-13 18:41:58 +0900

    compact

--- Formula/git-sensitive.rb
+++ Formula/git-sensitive.rb
@@ -1,8 +1,7 @@
 class GitSensitive < Formula
   desc "git sub-command to clean your sensitive content."
   homepage "https://github.com/ngtk/git-sensitive"
-  url "https://github.com/ngtk/git-sensitive/archive/v#{version}.tar.gz"
-  version "0.1.0"
+  url "https://github.com/ngtk/git-sensitive/archive/v0.1.0.tar.gz"
   sha256 "3996ae90f0c8a0e6f3f31df25badfa3f7fe35b22b3ae0ea9ca5fe137fce927d1"
 
   def install
